72ceea900d762fdeb280bc76bfc7267460a61681
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-03-14 09:02:16 +0900

    久しぶりのコミット

--- makeStrings.py
+++ makeStrings.py
@@ -1,16 +1,25 @@
 import matplotlib.pyplot as plt
 
 class interpData:
-    alphaArray = []
-    aArray = []
-    bArray = []
-    cArray = []
-    def __init__(self,fileName):
+    alphaArray = [] #alpha[0,1]
+    aArray = []     #position of Cl
+    bArray = []     #position of CH3
+    cArray = []     #position of Cl'
+    abArray = []    #length a-b
+    bcArray = []    #length b-c
+    valueArray = [] #value of potential or free energy
+    RCArray = []    #reaction coordinate
+    def __init__(self,fileName,fileNameOfValue):
         self.alphaArray = []
         self.aArray = []
         self.bArray = []
         self.cArray = []
+        self.abArray = []
+        self.bcArray = []
+        self.valueArray = []
+        self.RCArray = []
         self.fileName = fileName
+        self.fileNameOfValue = fileNameOfValue
         # print(self.aArray)
         # input()
         # print("My name is" + str(self.fileName))
@@ -28,23 +37,46 @@ class interpData:
         f.close()
         # print("ちゃんとデータ読めてるか確認" + str(len(self.aArray)))
         for i in range(len(self.aArray)):
-            self.cArray[i] -= self.bArray[i]
-            self.bArray[i] -= self.aArray[i]
+            self.abArray.append(self.bArray[i] - self.aArray[i])
+            self.bcArray.append(self.cArray[i] - self.bArray[i])
 
-    def plotValueAlongAlpha(self,colorName):
-        plt.plot(self.bArray,self.cArray,linewidth = 1,color = colorName)
+    def plotStringAlongAlpha(self,colorName):
+        plt.plot(self.abArray,self.bcArray,linewidth = 1,color = colorName)
         #plt.show()
 
+    def readValue(self):
+        f = open(self.fileNameOfValue,"r")
+        while True:
+            readLine = f.readline()
+            if readLine == "":
+                break
+            else:
+                alpha, value = map(float,readLine.split())
+                self.valueArray.append(value)
+        print(str(self.fileNameOfValue) + "  " + str(len(self.valueArray)))
+
+    def plotValueAlongRC(self,colorName):
+        for i in range(len(self.abArray)):
+            self.RCArray.append(self.abArray[i] - self.bcArray[i])
+        plt.plot(self.RCArray,self.valueArray, linewidth = 1, color = colorName)
 
 
 
-water = interpData("interpNewData.dat")
-water.plotValueAlongAlpha("green")
-air = interpData("interpAir.dat")
-air.plotValueAlongAlpha("red")
+water = interpData("interpNewData.dat","alongStringWater.dat")
+air = interpData("interpAir.dat","alongStringAir.dat")
 
-print(len(air.aArray))
-print(len(water.aArray))
-
-plt.savefig("strings.png")
+water.readValue()
+water.plotValueAlongRC("green")
+air.readValue()
+air.plotValueAlongRC("red")
+plt.savefig("valueAlongRC.png")
 plt.show()
+
+water.plotStringAlongAlpha("green")
+air.plotStringAlongAlpha("red")
+plt.show()
+
+# print(len(air.aArray))
+# print(len(water.aArray))
+
+# plt.savefig("strings.png")
